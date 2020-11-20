7e535fd9599f3d592e7db0527c931e570bd27d8f
Guilherme Klein Kern <guilherme.klein99@gmail.com>
2020-11-20 17:04:09 -0300

    Ver numero de chaves

--- src/main.cpp
+++ src/main.cpp
@@ -118,11 +118,8 @@ void TextRendering_PrintMatrixVectorProductDivW(GLFWwindow* window, glm::mat4 M,
 
 // Funções abaixo renderizam como texto na janela OpenGL algumas matrizes e
 // outras informações do programa. Definidas após main().
-void TextRendering_ShowModelViewProjection(GLFWwindow* window, glm::mat4 projection, glm::mat4 view, glm::mat4 model, glm::vec4 p_model);
-void TextRendering_ShowEulerAngles(GLFWwindow* window);
-void TextRendering_ShowProjection(GLFWwindow* window);
 void TextRendering_ShowFramesPerSecond(GLFWwindow* window);
-void TextRendering_ShowPosicaoJogador(GLFWwindow* window, Player player); //Teste
+void TextRendering_ShowChaves(GLFWwindow* window, Key r, Key g, Key b);
 
 // Funções callback para comunicação com o sistema operacional e interação do
 // usuário. Veja mais comentários nas definições das mesmas, abaixo.
@@ -412,13 +409,6 @@ int main(int argc, char* argv[])
 
         // Aqui executamos as operações de renderização
         glm::mat4 model = Matrix_Identity(); // Transformação identidade de modelagem
-        glm::mat4 modelBunny = Matrix_Identity(); // Transformação identidade do coelho
-        glm::mat4 modelCow = Matrix_Identity(); // Transformação identidade do coelho
-        glm::mat4 modelChaveVerde = Matrix_Identity(); // Transformação identidade do coelho
-        glm::mat4 modelChaveAzul = Matrix_Identity(); // Transformação identidade do coelho
-        glm::mat4 modelChaveVermelha = Matrix_Identity(); // Transformação identidade do coelho
-        glm::mat4 modeloParede = Matrix_Identity(); // Transformação identidade do coelho
-
 
         // Definimos a cor do "fundo" do framebuffer como branco.  Tal cor é
         // definida como coeficientes RGBA: Red, Green, Blue, Alpha; isto é:
@@ -604,9 +594,9 @@ int main(int argc, char* argv[])
             }
 
             // Desenhamos o modelo do personagem (coelho)
-            modelBunny = Matrix_Translate(player.position.x,player.position.y ,player.position.z)
+            model = Matrix_Translate(player.position.x,player.position.y ,player.position.z)
             * Matrix_Rotate_Y( g_CameraTheta - M_PI/2 + animation_coeficient );
-            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(modelBunny));
+            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
             glUniform1i(object_id_uniform, PERSONAGEM);
             DrawVirtualObject("bunny");
         }
@@ -618,50 +608,50 @@ int main(int argc, char* argv[])
         DrawVirtualObject("plane");
 
         // Desenhamos o modelo da vaca
-        modelCow = Matrix_Translate(0.0f,0.0f, 0.0f)
+        model = Matrix_Translate(0.0f,0.0f, 0.0f)
                  * Matrix_Scale( g_1KeyPressed ? 1.0f : 2.0f,
                                  g_2KeyPressed ? 1.0f : 2.0f,
                                  g_3KeyPressed ? 1.0f : 2.0f );
-        glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(modelCow));
+        glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
         glUniform1i(object_id_uniform, COW);
         DrawVirtualObject("cow");
 
         //Desenhamos as chaves
         if(!green_key.collected)
         {
-            modelChaveVerde = Matrix_Translate( green_key.sphere_collider.center.x,
+            model = Matrix_Translate( green_key.sphere_collider.center.x,
                                                 green_key.sphere_collider.center.y,
                                                 green_key.sphere_collider.center.z)
                             * Matrix_Rotate_Z(-4.72f)
                             * Matrix_Rotate_X((float)glfwGetTime())
                             * Matrix_Scale(0.25f,0.25f,0.25f);
-            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(modelChaveVerde));
+            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
             glUniform1i(object_id_uniform, CHAVE_VERDE);
             DrawVirtualObject("key");
         }
 
         if(!blue_key.collected)
         {
-            modelChaveAzul = Matrix_Translate(  blue_key.sphere_collider.center.x,
+            model = Matrix_Translate(  blue_key.sphere_collider.center.x,
                                                 blue_key.sphere_collider.center.y,
                                                 blue_key.sphere_collider.center.z)
                            * Matrix_Rotate_Z(-4.72f)
                            * Matrix_Rotate_X((float)glfwGetTime())
                            * Matrix_Scale(0.25f,0.25f,0.25f);
-            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(modelChaveAzul));
+            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
             glUniform1i(object_id_uniform, CHAVE_AZUL);
             DrawVirtualObject("key");
         }
 
         if(!red_key.collected)
         {
-            modelChaveVermelha = Matrix_Translate(  red_key.sphere_collider.center.x,
+            model = Matrix_Translate(  red_key.sphere_collider.center.x,
                                                     red_key.sphere_collider.center.y,
                                                     red_key.sphere_collider.center.z)
                                * Matrix_Rotate_Z(-4.72f)
                                * Matrix_Rotate_X((float)glfwGetTime())
                                * Matrix_Scale(0.25f,0.25f,0.25f);
-            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(modelChaveVermelha));
+            glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
             glUniform1i(object_id_uniform, CHAVE_VERMELHA);
             DrawVirtualObject("key");
         }
@@ -675,24 +665,9 @@ int main(int argc, char* argv[])
         for (auto fence: fences){
             DrawFences(fence, CERCA);
         }
-
-        // Pegamos um vértice com coordenadas de modelo (0.5, 0.5, 0.5, 1) e o
-        // passamos por todos os sistemas de coordenadas armazenados nas
-        // matrizes the_model, the_view, e the_projection; e escrevemos na tela
-        // as matrizes e pontos resultantes dessas transformações.
-        //glm::vec4 p_model(0.5f, 0.5f, 0.5f, 1.0f);
-        //TextRendering_ShowModelViewProjection(window, projection, view, model, p_model);
-
-        // Imprimimos na tela os ângulos de Euler que controlam a rotação do
-        // terceiro cubo.
-        TextRendering_ShowPosicaoJogador(window, player);
-        //TextRendering_ShowEulerAngles(window);
-
-        // Imprimimos na informação sobre a matriz de projeção sendo utilizada.
-        TextRendering_ShowProjection(window);
-
-        // Imprimimos na tela informação sobre o número de quadros renderizados
-        // por segundo (frames per second).
+        
+        // imprime na tela informacoes do jogo
+        TextRendering_ShowChaves(window, red_key, green_key, blue_key);
         TextRendering_ShowFramesPerSecond(window);
 
         // O framebuffer onde OpenGL executa as operações de renderização não
@@ -1633,70 +1608,8 @@ void ErrorCallback(int error, const char* description)
     fprintf(stderr, "ERROR: GLFW: %s\n", description);
 }
 
-// Esta função recebe um vértice com coordenadas de modelo p_model e passa o
-// mesmo por todos os sistemas de coordenadas armazenados nas matrizes model,
-// view, e projection; e escreve na tela as matrizes e pontos resultantes
-// dessas transformações.
-void TextRendering_ShowModelViewProjection(
-    GLFWwindow* window,
-    glm::mat4 projection,
-    glm::mat4 view,
-    glm::mat4 model,
-    glm::vec4 p_model
-)
-{
-    if ( !g_ShowInfoText )
-        return;
-
-    glm::vec4 p_world = model*p_model;
-    glm::vec4 p_camera = view*p_world;
-    glm::vec4 p_clip = projection*p_camera;
-    glm::vec4 p_ndc = p_clip / p_clip.w;
-
-    float pad = TextRendering_LineHeight(window);
-
-    TextRendering_PrintString(window, " Model matrix             Model     In World Coords.", -1.0f, 1.0f-pad, 1.0f);
-    TextRendering_PrintMatrixVectorProduct(window, model, p_model, -1.0f, 1.0f-2*pad, 1.0f);
-
-    TextRendering_PrintString(window, "                                        |  ", -1.0f, 1.0f-6*pad, 1.0f);
-    TextRendering_PrintString(window, "                            .-----------'  ", -1.0f, 1.0f-7*pad, 1.0f);
-    TextRendering_PrintString(window, "                            V              ", -1.0f, 1.0f-8*pad, 1.0f);
-
-    TextRendering_PrintString(window, " View matrix              World     In Camera Coords.", -1.0f, 1.0f-9*pad, 1.0f);
-    TextRendering_PrintMatrixVectorProduct(window, view, p_world, -1.0f, 1.0f-10*pad, 1.0f);
-
-    TextRendering_PrintString(window, "                                        |  ", -1.0f, 1.0f-14*pad, 1.0f);
-    TextRendering_PrintString(window, "                            .-----------'  ", -1.0f, 1.0f-15*pad, 1.0f);
-    TextRendering_PrintString(window, "                            V              ", -1.0f, 1.0f-16*pad, 1.0f);
-
-    TextRendering_PrintString(window, " Projection matrix        Camera                    In NDC", -1.0f, 1.0f-17*pad, 1.0f);
-    TextRendering_PrintMatrixVectorProductDivW(window, projection, p_camera, -1.0f, 1.0f-18*pad, 1.0f);
-
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-
-    glm::vec2 a = glm::vec2(-1, -1);
-    glm::vec2 b = glm::vec2(+1, +1);
-    glm::vec2 p = glm::vec2( 0,  0);
-    glm::vec2 q = glm::vec2(width, height);
-
-    glm::mat4 viewport_mapping = Matrix(
-        (q.x - p.x)/(b.x-a.x), 0.0f, 0.0f, (b.x*p.x - a.x*q.x)/(b.x-a.x),
-        0.0f, (q.y - p.y)/(b.y-a.y), 0.0f, (b.y*p.y - a.y*q.y)/(b.y-a.y),
-        0.0f , 0.0f , 1.0f , 0.0f ,
-        0.0f , 0.0f , 0.0f , 1.0f
-    );
-
-    TextRendering_PrintString(window, "                                                       |  ", -1.0f, 1.0f-22*pad, 1.0f);
-    TextRendering_PrintString(window, "                            .--------------------------'  ", -1.0f, 1.0f-23*pad, 1.0f);
-    TextRendering_PrintString(window, "                            V                           ", -1.0f, 1.0f-24*pad, 1.0f);
-
-    TextRendering_PrintString(window, " Viewport matrix           NDC      In Pixel Coords.", -1.0f, 1.0f-25*pad, 1.0f);
-    TextRendering_PrintMatrixVectorProductMoreDigits(window, viewport_mapping, p_ndc, -1.0f, 1.0f-26*pad, 1.0f);
-}
-
 // Escrevemos na tela a posição do jogador
-void TextRendering_ShowPosicaoJogador(GLFWwindow* window, Player player)
+void TextRendering_ShowChaves(GLFWwindow* window, Key r, Key g, Key b)
 {
     if ( !g_ShowInfoText )
         return;
@@ -1704,39 +1617,9 @@ void TextRendering_ShowPosicaoJogador(GLFWwindow* window, Player player)
     float pad = TextRendering_LineHeight(window);
 
     char buffer[80];
-    snprintf(buffer, 80, "Posicao do jogador = X(%.2f), Y(%.2f), Z(%.2f)\n", player.position.x, player.position.y, player.position.z);
+    snprintf(buffer, 80, "Chaves coletadas: %d/3\n", r.collected + g.collected + b.collected);
 
     TextRendering_PrintString(window, buffer, -1.0f+pad/10, -1.0f+2*pad/10, 1.0f);
-}
-
-// Escrevemos na tela os ângulos de Euler definidos nas variáveis globais
-// g_AngleX, g_AngleY, e g_AngleZ.
-void TextRendering_ShowEulerAngles(GLFWwindow* window)
-{
-    if ( !g_ShowInfoText )
-        return;
-
-    float pad = TextRendering_LineHeight(window);
-
-    char buffer[80];
-    snprintf(buffer, 80, "Euler Angles rotation matrix = Z(%.2f)*Y(%.2f)*X(%.2f)\n", g_AngleZ, g_AngleY, g_AngleX);
-
-    TextRendering_PrintString(window, buffer, -1.0f+pad/10, -1.0f+2*pad/10, 1.0f);
-}
-
-// Escrevemos na tela qual matriz de projeção está sendo utilizada.
-void TextRendering_ShowProjection(GLFWwindow* window)
-{
-    if ( !g_ShowInfoText )
-        return;
-
-    float lineheight = TextRendering_LineHeight(window);
-    float charwidth = TextRendering_CharWidth(window);
-
-    if ( g_UsePerspectiveProjection )
-        TextRendering_PrintString(window, "Perspective", 1.0f-13*charwidth, -1.0f+2*lineheight/10, 1.0f);
-    else
-        TextRendering_PrintString(window, "Orthographic", 1.0f-13*charwidth, -1.0f+2*lineheight/10, 1.0f);
 }
 
 // Escrevemos na tela o número de quadros renderizados por segundo (frames per
